3d7e2ccbdad02e0002d91048bf1d7035818c908e
Ahmed Talat <ahmedelsaeed105@gmail.com>
2017-08-26 08:42:09 +0300

    =before last

--- js/main.js
+++ js/main.js
@@ -40,16 +40,34 @@ $(window).ready(function(){
 	}
 });
 $(window).ready(function(){
-	$(".number2").animateNumber({"number":"18"});
+
+	var x =  $(".number2");
+	y=scrollMonitor.create(x);
+	y.enterViewport(function(){
+		$(".number2").animateNumber({"number":"18"});
+	});
+	
 });
 $(window).ready(function(){
-	$(".number1").animateNumber({"number":"99"});
+	var x =  $(".number1");
+	y=scrollMonitor.create(x);
+	y.enterViewport(function(){
+		$(".number1").animateNumber({"number":"99"});
+	});
 });
 $(window).ready(function(){
-	$(".number3").animateNumber({"number":"206"});
+	var x =  $(".number3");
+	y=scrollMonitor.create(x);
+	y.enterViewport(function(){
+		$(".number3").animateNumber({"number":"206"});
+	});
 });
 $(window).ready(function(){
-	$(".number4").animateNumber({"number":"73"});
+	var x =  $(".number4");
+	y=scrollMonitor.create(x);
+	y.enterViewport(function(){
+		$(".number4").animateNumber({"number":"73"});
+	});
 });
 
 $("#image1").mouseover(function(){
@@ -213,13 +231,93 @@ $("#inputChange").on("blur",function(){
 // $("#linkStyle").css({"color":"white"});
 
 // });
+$(document).on("scroll",function(){
+	if($(this).scrollTop() > 100){
+		$("#row").addClass("classMenu");
+		$("#row").addClass("fixed-top");
+		$(".styleLink2").addClass("addedMenu");
+		$(".styleLink2").css("color","#222");
+		$("#row").css("margin-top","0px");
+		$(".homeChild").css("left","25%");
+		if($(window).width() <= 1280){
+			$(".homeChild").css("left","20%");
+		}
+		$(".homeChild").css("top","99%");
+		$(".servicesChild").css("top","99%");
+		$(".portfolioChild").css("top","99%");
+		$("#menuImage").attr("src","http://webnus.biz/themes/easyweb/host/wp-content/uploads/2016/01/stk-logo-1.png");
+		$("#menuImage").css("margin-top","3%");
+	}
+	else{
+		$("#row").removeClass("classMenu");
+		$("#row").removeClass("fixed-top");
+		$(".styleLink2").removeClass("addedMenu");
+		$("#row").css("margin-top","2%");
+		$(".styleLink2").css("color","white");
+		$("#menuImage").attr("src","http://webnus.net/themes/easyweb/design/wp-content/uploads/2016/02/logo2d-w-1.png");
+		$("#menuImage").css("margin-top","0%");
+		$(".homeChild").css("left","24%");
+		if($(window).width() <= 1280){
+			$(".homeChild").css("left","19%");
+		}
+		$(".homeChild").css("top","90%");
+		$(".servicesChild").css("top","90%");
+		$(".portfolioChild").css("top","90%");
 
+
+	}
+});
 		
 
 
 
 
 
+$("#pag1").click(function(){
+	$(this).css("border"," 3px solid #00c2e5");
+		$("#ILLUSTRATIONS").css("border","3pxshow solid #e9e9e9");
+$("#all").css("border","3px solid #e9e9e9");
+$("#mobile").css("border","3px solid #e9e9e9");
+$("#pag2").css("border","3px solid #e9e9e9");
+
+	$("#image2").show("scale",800);
+	$("#image3").show("scale",800);
+	$("#image4").show("scale",800);
+	$("#image5").show("scale",800);
+	$("#image6").show("scale",800);
+	$("#image7").hide("scale",800);
+	$("#image8").hide("scale",800);
+	$("#image9").hide("scale",800);
+	$(".backgroundHover").css({"opacity":"0"});
+	$(".xx").css({"opacity":"0"});
+	// $("#filterByAnimation").css({"height":"auto"});
+});
 
 
-	
+$("#pag2").click(function(){
+	$(this).css("border"," 3px solid #00c2e5");
+		$("#ILLUSTRATIONS").css("border","3pxshow solid #e9e9e9");
+$("#all").css("border","3px solid #e9e9e9");
+$("#mobile").css("border","3px solid #e9e9e9");
+$("#pag1").css("border","3px solid #e9e9e9");
+
+	$("#image2").hide("scale",800);
+	$("#image3").hide("scale",800);
+	$("#image4").hide("scale",800);
+	$("#image5").hide("scale",800);
+	$("#image6").hide("scale",800);
+	$("#image7").show("scale",800);
+	$("#image8").show("scale",800);
+	$("#image9").show("scale",800);
+	$(".backgroundHover").css({"opacity":"0"});
+	$(".xx").css({"opacity":"0"});
+	// $("#filterByAnimation").css({"height":"auto"});
+});
+
+	$(document).ready(function(){
+		if($(this).width() <= 767){
+			$("#text101").addClass("text-center");
+				$("#text105").removeClass("text-right");
+					$("#text105").addClass("text-center");
+		}
+	})
